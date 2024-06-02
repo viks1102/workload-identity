71a399233057b6af32c41c771258b9dffec8271e
viks1102 <viks_1102@yahoo.com>
2024-06-03 00:58:06 +0100

    feat: assigned bucket list access

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
-output "kubernetes_service_account_name" {
-  description = "The name of the Kubernetes service account"
-  value       = kubernetes_service_account.k8s_service_account.metadata[0].name
-}
+# output "kubernetes_service_account_name" {
+#   description = "The name of the Kubernetes service account"
+#   value       = kubernetes_service_account.k8s_service_account.metadata[0].name
+# }
 
 output "google_service_account_email" {
   description = "The email of the Google Service Account"
